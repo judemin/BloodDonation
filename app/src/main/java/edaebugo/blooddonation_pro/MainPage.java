829e5ef1540d20823682476f66545c57f244e0ea
judemin <judemin2087@naver.com>
2019-07-29 20:12:01 +0900

    20190729_0811
    Added Mainpage

--- app/src/main/java/edaebugo/blooddonation_pro/MainPage.java
+++ app/src/main/java/edaebugo/blooddonation_pro/MainPage.java
@@ -1,4 +1,19 @@
 package edaebugo.blooddonation_pro;
 
-public class MainPage {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainPage  extends AppCompatActivity{
+    public String param;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+        Intent myIntent = getIntent();
+        param = myIntent.getStringExtra("tmp");
+        Toast.makeText(getApplicationContext(),param,Toast.LENGTH_LONG).show();
+    }
 }
