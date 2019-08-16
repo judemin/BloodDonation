75aefb4694574fb2ac8853c90c0e4c97c4d73533
GraceGH <47500172+GraceGH@users.noreply.github.com>
2019-08-16 15:22:26 +0900

    20190816
    GraceGH Commit Test

--- opencv/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ opencv/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\user1\\Documents\\GitHub\\BloodDonation\\opencv\\java\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\gahyu\\Desktop\\OCRtest\\BloodDonation\\opencv\\java\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
